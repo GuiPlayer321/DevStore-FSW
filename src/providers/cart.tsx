182dd0f92f4d39e99102c9628afa2609bfb680fc
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-29 22:33:30 -0300

    feat: add persistence to cart products

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -2,7 +2,7 @@
 
 import { ProductWithTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
-import { ReactNode, createContext, useMemo, useState } from "react";
+import { ReactNode, createContext, useEffect, useMemo, useState } from "react";
 
 export interface CartProduct extends ProductWithTotalPrice{
     quantity: number
@@ -38,7 +38,11 @@ export const CartContext = createContext<ICartContext>({
 
 const CartProvider = ({children}: {children: ReactNode}) =>{
 
-    const [products, setProducts] = useState<CartProduct[]>([])
+    const [products, setProducts] = useState<CartProduct[]>([JSON.parse(localStorage.getItem("@ecommerce:cart") || "[]")])
+    
+    useEffect(()=>{
+        localStorage.setItem("@ecommerce:cart", JSON.stringify(products))
+    }, [products])
 
     const subtotal = useMemo(()=>{
         return products.reduce((acc, product) => {
