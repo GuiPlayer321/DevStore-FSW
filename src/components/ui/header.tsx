be958342ba4f356a04e7aff76c74e1eac155c8fb
Guilherme de Carvalho Ferreira <64448040+GuiPlayer321@users.noreply.github.com>
2023-10-29 22:46:28 -0300

    feat: add deals page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -69,7 +69,11 @@ const Header = () => {
                         </Link>
                     </SheetClose>
 
+                    <SheetClose asChild>
+                        <Link href={'/deals'}>
                     <Button variant={"outline"} className="w-full justify-start gap-2"><PercentIcon size={16} /> Ofertas</Button>
+                    </Link>
+                    </SheetClose>
                     
                     <SheetClose asChild>
                         <Link href={'/catalog'}>
